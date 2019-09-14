165cc4ab4956b35caf0e9c20ecd7359409ef3df6
matteuc <ymc006@ucsd.edu>
2019-09-14 10:50:52 -0700

    added routing and handling GET requests

---
+++ server.js
@@ -0,0 +1,66 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Application Variables 
+var MAX_TABLES = 5;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/reservations", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+// Displays all characters
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations);
+});
+
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/reservations", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+
+    console.log(newReservation);
+
+    if (reservations.length < MAX_TABLES) {
+        reservations.push(newReservation);
+    } else {
+        waitlist.push(newReservation);
+    }
+
+    res.json(newReservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
